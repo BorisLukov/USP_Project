dfda44681720324daeb088691a793edadb1d2259
StoyanOrtsev <35624797+StoyanOrtsev@users.noreply.github.com>
2023-05-13 19:35:40 +0300

    Business logic.
    Filtering, sorting and display functionality.

--- web_project_main.php
+++ web_project_main.php
@@ -75,5 +75,91 @@
     <div class="apartments">
         <?php
         include "config.php";
+        $sql = "SELECT * FROM Estates"
+                    . " inner join Cities on Estates.city_id=Cities.city_id"
+                    . " inner join Types_of_estates on Estates.estate_type_id=Types_of_estates.estate_type_id"
+                    . " inner join Sellers on Estates.seller_id=Sellers.seller_id";
+        if (isset($_POST["filter"]))
+        {
+            $city = $_POST["city"];
+            $price1 = (float)$_POST["price"];
+            $price2 = (float)$price1 + 19999;
+            $rooms = $_POST["rooms"];
+            
+            if($price1 >= 110000 || $price1 == 0){$price2 = 999999;}
+            $filter = $sql." where Estates.price between $price1 and $price2";
+
+            if($city != 'none'){
+                $filter." and Cities.city_name = '$city'";
+            }
+
+            if($rooms != 'none'){
+                $filter." and Estates.rooms = $rooms";
+            }
+            
+            $sort_attr=$_POST['sorting'];
+                if($sort_attr == 'ascending'){
+                    $filter .=" order by Estates.price asc";
+                }
+                elseif($sort_attr == 'descending'){
+                    $filter.=" order by Estates.price desc";
+                }
+                elseif($sort_attr == 'newest'){
+                    $filter.=" order by Estates.estate_id asc";
+                }
+                elseif($sort_attr == 'oldest'){
+                    $filter.=" order by Estates.estate_id desc";
+                }
+            
+                $result =mysqli_query($dbConn, $filter);
+                if($result->num_rows)
+                {
+                    while($row = mysqli_fetch_assoc($result))
+                    {
+                        $show = "<b><h2>Estate price: ".$row['price']."</b></h2><h3>".
+                                "Quadrature: ".$row['quadrature'].", Number of rooms: ".$row['rooms'].", Address: ".$row['estate_address'].", City: ".$row['city_name'].
+                                "<br> Estate type: ".$row['type_name']." Seller name: ".$row['seller_name'].", Telephone: ".$row['telephone'];
+                        if($row['furnished']==1) {echo $show.", Furnished: Yes</h3>";}
+                        else {echo $show.", Furnished: No</h3>";}
+                    }
+                }
+        }
+        else
+        {
+            
+            if(isset($_POST['sort'])){
+                $sort_attr=$_POST['sorting'];
+                if($sort_attr == 'ascending'){
+                    $sql .=" order by Estates.price asc";
+                }
+                elseif($sort_attr == 'descending'){
+                    $sql.=" order by Estates.price desc";
+                }
+                elseif($sort_attr == 'newest'){
+                    $sql.=" order by Estates.estate_id asc";
+                }
+                elseif($sort_attr == 'oldest'){
+                    $sql.=" order by Estates.estate_id desc";
+                }
+            }
+            
+            $result =mysqli_query($dbConn, $sql);
+            if($result->num_rows)
+            {
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    $show = "<b><h2>Estate price: ".$row['price']."</b></h2><h3>".
+                            "Quadrature: ".$row['quadrature'].", Number of rooms: ".$row['rooms'].", Address: ".$row['estate_address'].", City: ".$row['city_name'].
+                            "<br> Estate type: ".$row['type_name']." Seller name: ".$row['seller_name'].", Telephone: ".$row['telephone'];
+                    if($row['furnished']==1) {echo $show.", Furnished: Yes<br><hr></h3>";}
+                    else {echo $show.", Furnished: No<br><hr></h3>";}
+                }
+            }
+            else
+            {echo "No results";}
+        }
         ?>
     </div>
+        
+</body>
+</html>
